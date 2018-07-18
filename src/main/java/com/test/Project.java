751c347d795c368d7da476279e5ed2e09fedc608
1 <1>
2018-07-18 09:54:07 -0400

    first normal commit

---
+++ src/main/java/com/test/Project.java
@@ -0,0 +1,16 @@
+package com.test;
+
+//dependency on Apache commons-collections
+import org.apache.commons.collections.Predicate;
+
+class Project {
+
+  public static void test() {
+    System.out.println("test");
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Hellow MTN");
+  }
+
+}
---
+++ src/test/java/com/test/UnitTest.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import org.junit.Test;
+
+public class UnitTest {
+   
+   @Test
+   public void test(){
+     Project.test();
+   }
+}
