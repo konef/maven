a15d15839c800e9574f98f73ba05176dc9139f1d
konef <40858004+konef@users.noreply.github.com>
2018-07-27 19:25:18 +0300

    Update Project.java

--- src/main/java/com/test/Project.java
+++ src/main/java/com/test/Project.java
@@ -11,6 +11,11 @@ class Project {
   
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
+    
+    
+    hello maven
+      yuo willn`t work
+      ha ha ha
   }
 
 }
